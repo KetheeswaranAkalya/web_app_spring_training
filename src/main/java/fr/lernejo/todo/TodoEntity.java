8655a3041da67e523fd88b915da2c2153e479e85
KetheeswaranAkalya <ketheeswaran@et.esiea.fr>
2023-02-14 16:25:45 +0100

    part7updated

--- src/main/java/fr/lernejo/todo/TodoEntity.java
+++ src/main/java/fr/lernejo/todo/TodoEntity.java
@@ -1,11 +1,12 @@
 package fr.lernejo.todo;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Table("todo")
 public class TodoEntity {
     public String message;
     public String author;
-
+@Id
     public Long id;
 }
--- src/main/java/fr/lernejo/todo/TodoListController.java
+++ src/main/java/fr/lernejo/todo/TodoListController.java
@@ -3,21 +3,26 @@ package fr.lernejo.todo;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TodoListController {
-    private final List<Todo> todolist = new ArrayList<>();
+    private final TodoRepository todoRepository;
 
+    public TodoListController(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
 
     @GetMapping("/api/todo")
-  public List<Todo> todolist(){return todolist;}
-    @PostMapping("/api/todo")
-  public void addList(String author, String message) {
-      todolist.add(new Todo(author, message));
-  }
+  public TodoRepository todolist(){return todoRepository;}
 
+    @PostMapping("/api/todo")
+
+    public void addList(@RequestBody Todo todo) {
+        TodoEntity do_item = new TodoEntity();
+        do_item.message = todo.message();
+        do_item.author = todo.author();
+        todoRepository.save(do_item);
+    }
 
 }
